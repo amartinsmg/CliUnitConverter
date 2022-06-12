ce85cc82f75dab6b24221d54d361773c66a06d90
amartinsmg <andremindurimg@gmail.com>
2022-06-12 10:22:12 -0300

    program update

--- src/program.pas
+++ src/program.pas
@@ -4,8 +4,7 @@ var
   value, result: Real;
 
   function Length(inputUnit, outputUnit: Integer; value: Real) :Real;
-  var
-    m: Real;
+  var m: Real;
   begin
     case inputUnit of
       1: m := value; // m
@@ -30,8 +29,7 @@ var
   end;
 
   function Area(inputUnit, outputUnit: Integer; value: Real) :Real;
-  var
-    m2: Real;
+  var m2: Real;
   begin
     case inputUnit of
       1: m2 := value; // m2
@@ -236,146 +234,146 @@ var
   end;
 begin
   repeat
-    writeln('           UNIT CONVERTER          ');
-    writeln('               *****               ');
-    writeln('Which conversion do you want to do?');
-    writeln('TYPE                           CODE');
-    writeln('Length                           1 ');
-    writeln('Area                             2 ');
-    writeln('Volume                           3 ');
-    writeln('Mass                             4 ');
-    writeln('Pressure                         5 ');
-    writeln('Time                             6 ');
-    writeln('Speed                            7 ');
-    writeln('Acceleration                     8 ');
-    writeln('Force                            9 ');
-    writeln('Temperature                     10 ');
-    writeln('Energy                          11 ');
-    writeln('Power                           12 ');
-    writeln('               *****               ');
+    writeln('                  UNIT CONVERTER                 ');
+    writeln('                      *****                      ');
+    writeln('Which conversion do you want to do?'              );
+    writeln('TYPE                                         CODE');
+    writeln('Length                                         1 ');
+    writeln('Area                                           2 ');
+    writeln('Volume                                         3 ');
+    writeln('Mass                                           4 ');
+    writeln('Pressure                                       5 ');
+    writeln('Time                                           6 ');
+    writeln('Speed                                          7 ');
+    writeln('Acceleration                                   8 ');
+    writeln('Force                                          9 ');
+    writeln('Temperature                                   10 ');
+    writeln('Energy                                        11 ');
+    writeln('Power                                         12 ');
+    writeln('                       *****                     ');
     write('Enter a code: ');
     read(opcode);
     writeln(' ');
     case opcode of
       1: begin
-        writeln('               LENGTH              ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('meter (m)                        1 ');
-        writeln('millimeter (mm)                  2 ');
-        writeln('centimeter (cm)                  3 ');
-        writeln('inch (in)                        4 ');
-        writeln('foot (ft)                        5 ');
-        writeln('kilometer (km)                   6 ');
-        writeln('mile (mi)                        7 ');
-        writeln('nautical mile (nmi)              8 ');
+        writeln('                      LENGTH                     ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('meter (m)                                      1 ');
+        writeln('millimeter (mm)                                2 ');
+        writeln('centimeter (cm)                                3 ');
+        writeln('inch (in)                                      4 ');
+        writeln('foot (ft)                                      5 ');
+        writeln('kilometer (km)                                 6 ');
+        writeln('mile (mi)                                      7 ');
+        writeln('nautical mile (nmi)                            8 ');
       end;
       2: begin
-        writeln('                AREA               ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('square meter (m^2)               1 ');
-        writeln('square inch (in^2)               2 ');
-        writeln('square foot (ft^2)               3 ');
-        writeln('hectare (ha)                     4 ');
-        writeln('square kilometer (km^2)          5 ');
-        writeln('square mile (mi^2)               6 ');
+        writeln('                       AREA                      ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('square meter (m^2)                             1 ');
+        writeln('square inch (in^2)                             2 ');
+        writeln('square foot (ft^2)                             3 ');
+        writeln('hectare (ha)                                   4 ');
+        writeln('square kilometer (km^2)                        5 ');
+        writeln('square mile (mi^2)                             6 ');
       end;
       3: begin
-        writeln('               VOLUME              ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('cubic meter (m^3)                1 ');
-        writeln('milliliter (mL or cm^3)          2 ');
-        writeln('cubic inch (in^3)                3 ');
-        writeln('fluid ounce (fl oz)              4 ');
-        writeln('liter (L)                        5 ');
-        writeln('imperial gallon (imp gal)        6 ');
-        writeln('cubic foot (ft^3)                7 ');
+        writeln('                      VOLUME                     ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('cubic meter (m^3)                              1 ');
+        writeln('milliliter (mL or cm^3)                        2 ');
+        writeln('cubic inch (in^3)                              3 ');
+        writeln('fluid ounce (fl oz)                            4 ');
+        writeln('liter (L)                                      5 ');
+        writeln('imperial gallon (imp gal)                      6 ');
+        writeln('cubic foot (ft^3)                              7 ');
       end;
       4: begin
-        writeln('                MASS               ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('kilogram (kg)                    1 ');
-        writeln('milligram (mg)                   2 ');
-        writeln('gram (g)                         3 ');
-        writeln('ounce (oz)                       4 ');
-        writeln('pound (lb)                       5 ');
+        writeln('                       MASS                      ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('kilogram (kg)                                  1 ');
+        writeln('milligram (mg)                                 2 ');
+        writeln('gram (g)                                       3 ');
+        writeln('ounce (oz)                                     4 ');
+        writeln('pound (lb)                                     5 ');
       end;
       5: begin
-        writeln('              PRESSURE             ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('pascal (Pa)                      1 ');
-        writeln('psi                              2 ');
-        writeln('bar                              3 ');
-        writeln('standard atmosphere (atm)        4 ');
+        writeln('                     PRESSURE                    ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('pascal (Pa)                                    1 ');
+        writeln('psi                                            2 ');
+        writeln('bar                                            3 ');
+        writeln('standard atmosphere (atm)                      4 ');
       end;
       6: begin
-        writeln('                TIME               ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('second (s)                       1 ');
-        writeln('minute (min)                     2 ');
-        writeln('hour (h)                         3 ');
-        writeln('day (d)                          4 ');
-        writeln('year (y)                         5 ');
+        writeln('                       TIME                      ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('second (s)                                     1 ');
+        writeln('minute (min)                                   2 ');
+        writeln('hour (h)                                       3 ');
+        writeln('day (d)                                        4 ');
+        writeln('year (y)                                       5 ');
       end;
       7: begin
-        writeln('               SPEED               ');
-        writeln('               *****               ');
-        writeln('UNIT                           CODE');
-        writeln('meter/second (m/s)               1 ');
-        writeln('foot/minute (ft/min)             2 ');
-        writeln('kilometer/hour (km/h)            3 ');
-        writeln('mile/hour (m/hi)                 4 ');
-        writeln('knot (kn or kt)                  5 ');
+        writeln('                       SPEED                     ');
+        writeln('                       *****                     ');
+        writeln('UNIT                                         CODE');
+        writeln('meter/second (m/s)                             1 ');
+        writeln('foot/minute (ft/min)                           2 ');
+        writeln('kilometer/hour (km/h)                          3 ');
+        writeln('mile/hour (m/hi)                               4 ');
+        writeln('knot (kn or kt)                                5 ');
       end;
       8: begin
-        writeln('            ACCELERATION           ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('meter/square second (m/s^2)      1 ');
-        writeln('kilometer/hour*second (km/h*s)   2 ');
-        writeln('foot/square second (ft/s^2)      3 ');
-        writeln('knot/second (kn/s or kt/s)       4 ');
-        writeln('acceleration of gravity (G)      5 ');
+        writeln('                   ACCELERATION                  ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('meter/square second (m/s^2)                    1 ');
+        writeln('kilometer/hour*second (km/h*s)                 2 ');
+        writeln('foot/square second (ft/s^2)                    3 ');
+        writeln('knot/second (kn/s or kt/s)                     4 ');
+        writeln('acceleration of gravity (G)                    5 ');
       end;
       9: begin
-        writeln('               FORCE               ');
-        writeln('               *****               ');
-        writeln('UNIT                           CODE');
-        writeln('newton (N)                       1 ');
-        writeln('dyne (dyn)                       2 ');
-        writeln('pound-force (lbf)                3 ');
-        writeln('kilometer-force (kmf)            4 ');
+        writeln('                       FORCE                     ');
+        writeln('                       *****                     ');
+        writeln('UNIT                                         CODE');
+        writeln('newton (N)                                     1 ');
+        writeln('dyne (dyn)                                     2 ');
+        writeln('pound-force (lbf)                              3 ');
+        writeln('kilometer-force (kmf)                          4 ');
       end;
       10: begin
-        writeln('            TEMPERATURE            ');
-        writeln('               *****               ');
-        writeln('UNIT                           CODE');
-        writeln('kelvin (k)                       1 ');
-        writeln('Celsius (°C)                     2 ');
-        writeln('Fahrenheit (°F)                  3 ');
+        writeln('                   TEMPERATURE                   ');
+        writeln('                      *****                      ');
+        writeln('UNIT                                         CODE');
+        writeln('kelvin (k)                                     1 ');
+        writeln('Celsius (o C)                                  2 ');
+        writeln('Fahrenheit (o F)                               3 ');
       end;
       11: begin
-        writeln('               ENERGY              ');
-        writeln('               ******              ');
-        writeln('UNIT                           CODE');
-        writeln('joule (J)                        1 ');
-        writeln('kilojoule (kJ)                   2 ');
-        writeln('kilocalorie (kcal)               3 ');
-        writeln('kilowatt/hour (kw/h)             4 ');
+        writeln('                      ENERGY                     ');
+        writeln('                      ******                     ');
+        writeln('UNIT                                         CODE');
+        writeln('joule (J)                                      1 ');
+        writeln('kilojoule (kJ)                                 2 ');
+        writeln('kilocalorie (kcal)                             3 ');
+        writeln('kilowatt/hour (kw/h)                           4 ');
       end;
       12: begin
-        writeln('               POWER               ');
-        writeln('               *****               ');
-        writeln('UNIT                           CODE');
-        writeln('watt (W)                         1 ');
-        writeln('kilojoule/hour (kJ/h)            2 ');
-        writeln('hoursepower (hp)                 3 ');
-        writeln('kilowatt (kW)                    4 ');
+        writeln('                      POWER                      ');
+        writeln('                      *****                      ');
+        writeln('UNIT                                         CODE');
+        writeln('watt (W)                                       1 ');
+        writeln('kilojoule/hour (kJ/h)                          2 ');
+        writeln('hoursepower (hp)                               3 ');
+        writeln('kilowatt (kW)                                  4 ');
       end;
     end;
     writeln(' ');
