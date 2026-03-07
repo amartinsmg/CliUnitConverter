2de334150e5f5508a1b33e62540b8fb7694edb70
amartinsmg <andremindurimg@gmail.com>
2026-03-07 14:15:28 -0300

    Update program.pas: refactor keywords to follow modern conventions

--- src/program.pas
+++ src/program.pas
@@ -1,160 +1,160 @@
 
-Program UnitConverter;
+program UnitConverter;
 
 {$mode objfpc}
 
-Uses Calculate, SysUtils;
+uses Calculate, SysUtils;
 
-Procedure MainMenu();
-Begin
-  Writeln(' UNIT CONVERTER' + sLineBreak);
-  Writeln('  1  -  Length');
-  Writeln('  2  -  Area');
-  Writeln('  3  -  Volume');
-  Writeln('  4  -  Mass');
-  Writeln('  5  -  Pressure');
-  Writeln('  6  -  TimeC');
-  Writeln('  7  -  Speed');
-  Writeln('  8  -  Acceleration');
-  Writeln('  9  -  Force');
-  Writeln('  10 -  Temperature');
-  Writeln('  11 -  Energy');
-  Writeln('  12 -  Power');
-  Writeln('  13 -  Quit' + sLineBreak);
-End;
+procedure MainMenu();
+begin
+  WriteLn(' UNIT CONVERTER' + sLineBreak);
+  WriteLn('  1  -  Length');
+  WriteLn('  2  -  Area');
+  WriteLn('  3  -  Volume');
+  WriteLn('  4  -  Mass');
+  WriteLn('  5  -  Pressure');
+  WriteLn('  6  -  TimeC');
+  WriteLn('  7  -  Speed');
+  WriteLn('  8  -  Acceleration');
+  WriteLn('  9  -  Force');
+  WriteLn('  10 -  Temperature');
+  WriteLn('  11 -  Energy');
+  WriteLn('  12 -  Power');
+  WriteLn('  13 -  Quit' + sLineBreak);
+end;
 
 
-Procedure Menus(Opcode: LongInt);
-Begin
-  Case Opcode Of 
+procedure Menus(Opcode: LongInt);
+begin
+  case Opcode of 
     1:
-       Begin
-         Writeln(' LENGTH' + sLineBreak);
-         Writeln('  1 -  meter (m)');
-         Writeln('  2 -  millimeter (mm)');
-         Writeln('  3 -  centimeter (cm)');
-         Writeln('  4 -  inch (in)');
-         Writeln('  5 -  foot (ft)');
-         Writeln('  6 -  kilometer (km)');
-         Writeln('  7 -  mile (mi)');
-         Writeln('  8 -  nautical mile (nmi)');
-       End;
+       begin
+         WriteLn(' LENGTH' + sLineBreak);
+         WriteLn('  1 -  meter (m)');
+         WriteLn('  2 -  millimeter (mm)');
+         WriteLn('  3 -  centimeter (cm)');
+         WriteLn('  4 -  inch (in)');
+         WriteLn('  5 -  foot (ft)');
+         WriteLn('  6 -  kilometer (km)');
+         WriteLn('  7 -  mile (mi)');
+         WriteLn('  8 -  nautical mile (nmi)');
+       end;
     2:
-       Begin
-         Writeln(' AREA' + sLineBreak);
-         Writeln('  1 -  square meter (m^2)');
-         Writeln('  2 -  square centimeter (cm^2)');
-         Writeln('  3 -  square inch (in^2)');
-         Writeln('  4 -  square foot (ft^2)');
-         Writeln('  5 -  hectare (ha)');
-         Writeln('  6 -  square kilometer (km^2)');
-         Writeln('  7 -  square mile (mi^2)');
-       End;
+       begin
+         WriteLn(' AREA' + sLineBreak);
+         WriteLn('  1 -  square meter (m^2)');
+         WriteLn('  2 -  square centimeter (cm^2)');
+         WriteLn('  3 -  square inch (in^2)');
+         WriteLn('  4 -  square foot (ft^2)');
+         WriteLn('  5 -  hectare (ha)');
+         WriteLn('  6 -  square kilometer (km^2)');
+         WriteLn('  7 -  square mile (mi^2)');
+       end;
     3:
-       Begin
-         Writeln(' VOLUME' + sLineBreak);
-         Writeln('  1 -  cubic meter (m^3)');
-         Writeln('  2 -  milliliter or cubic centimeter (mL or cm^3)');
-         Writeln('  3 -  cubic inch (in^3)');
-         Writeln('  4 -  fluid ounce(US) (fl oz)');
-         Writeln('  5 -  liter (L)');
-         Writeln('  6 -  gallon(US) (gal)');
-         Writeln('  7 -  cubic foot (ft^3)');
-       End;
+       begin
+         WriteLn(' VOLUME' + sLineBreak);
+         WriteLn('  1 -  cubic meter (m^3)');
+         WriteLn('  2 -  milliliter or cubic centimeter (mL or cm^3)');
+         WriteLn('  3 -  cubic inch (in^3)');
+         WriteLn('  4 -  fluid ounce(US) (fl oz)');
+         WriteLn('  5 -  liter (L)');
+         WriteLn('  6 -  gallon(US) (gal)');
+         WriteLn('  7 -  cubic foot (ft^3)');
+       end;
     4:
-       Begin
-         Writeln(' MASS' + sLineBreak);
-         Writeln('  1 -  kilogram (kg)');
-         Writeln('  2 -  milligram (mg)');
-         Writeln('  3 -  gram (g)');
-         Writeln('  4 -  ounce (oz)');
-         Writeln('  5 -  pound (lb)');
-         Writeln('  6 -  tonne (t)');
-       End;
+       begin
+         WriteLn(' MASS' + sLineBreak);
+         WriteLn('  1 -  kilogram (kg)');
+         WriteLn('  2 -  milligram (mg)');
+         WriteLn('  3 -  gram (g)');
+         WriteLn('  4 -  ounce (oz)');
+         WriteLn('  5 -  pound (lb)');
+         WriteLn('  6 -  tonne (t)');
+       end;
     5:
-       Begin
-         Writeln(' PRESSURE' + sLineBreak);
-         Writeln('  1 -  Pascal or Newton per square meter (Pa or N/m^2)');
-         Writeln('  2 -  millimeter of mercury (mmHg)');
-         Writeln('  3 -  psi');
-         Writeln('  4 -  bar');
-         Writeln('  5 -  standard atmosphere (atm)');
-       End;
+       begin
+         WriteLn(' PRESSURE' + sLineBreak);
+         WriteLn('  1 -  Pascal or Newton per square meter (Pa or N/m^2)');
+         WriteLn('  2 -  millimeter of mercury (mmHg)');
+         WriteLn('  3 -  psi');
+         WriteLn('  4 -  bar');
+         WriteLn('  5 -  standard atmosphere (atm)');
+       end;
     6:
-       Begin
-         Writeln(' TIME' + sLineBreak);
-         Writeln('  1 -  second (s)');
-         Writeln('  2 -  minute (min)');
-         Writeln('  3 -  hour (h)');
-         Writeln('  4 -  day (d)');
-         Writeln('  5 -  year (y)');
-       End;
+       begin
+         WriteLn(' TIME' + sLineBreak);
+         WriteLn('  1 -  second (s)');
+         WriteLn('  2 -  minute (min)');
+         WriteLn('  3 -  hour (h)');
+         WriteLn('  4 -  day (d)');
+         WriteLn('  5 -  year (y)');
+       end;
     7:
-       Begin
-         Writeln(' SPEED' + sLineBreak);
-         Writeln('  1 -  meter per second (m/s)');
-         Writeln('  2 -  foot per minute (ft/min)');
-         Writeln('  3 -  kilometer per hour (km/h)');
-         Writeln('  4 -  mile per hour (m/hi)');
-         Writeln('  5 -  knot or nautical mile per hour (kn or kt or nmi/h)');
-       End;
+       begin
+         WriteLn(' SPEED' + sLineBreak);
+         WriteLn('  1 -  meter per second (m/s)');
+         WriteLn('  2 -  foot per minute (ft/min)');
+         WriteLn('  3 -  kilometer per hour (km/h)');
+         WriteLn('  4 -  mile per hour (m/hi)');
+         WriteLn('  5 -  knot or nautical mile per hour (kn or kt or nmi/h)');
+       end;
     8:
-       Begin
-         Writeln(' ACCELERATION' + sLineBreak);
-         Writeln('  1 -  meter per square second (m/s^2)');
-         Writeln('  2 -  foot per min per second (ft/min/s)');
-         Writeln('  3 -  kilometer per hour per second (km/h/s)');
-         Writeln('  4 -  mile per hour per second (mi/h/s)');
-         Writeln('  5 -  knot per second (kn/s or kt/s)');
-         Writeln('  6 -  acceleration of gravity (G)');
-       End;
+       begin
+         WriteLn(' ACCELERATION' + sLineBreak);
+         WriteLn('  1 -  meter per square second (m/s^2)');
+         WriteLn('  2 -  foot per min per second (ft/min/s)');
+         WriteLn('  3 -  kilometer per hour per second (km/h/s)');
+         WriteLn('  4 -  mile per hour per second (mi/h/s)');
+         WriteLn('  5 -  knot per second (kn/s or kt/s)');
+         WriteLn('  6 -  acceleration of gravity (G)');
+       end;
     9:
-       Begin
-         Writeln(' FORCE' + sLineBreak);
-         Writeln('  1 -  Newton (N)');
-         Writeln('  2 -  dyne (dyn)');
-         Writeln('  3 -  pound-force (lbf)');
-         Writeln('  4 -  kilometer-force (kgf)');
-       End;
+       begin
+         WriteLn(' FORCE' + sLineBreak);
+         WriteLn('  1 -  Newton (N)');
+         WriteLn('  2 -  dyne (dyn)');
+         WriteLn('  3 -  pound-force (lbf)');
+         WriteLn('  4 -  kilometer-force (kgf)');
+       end;
     10:
-        Begin
-          Writeln(' TEMPERATURE' + sLineBreak);
-          Writeln('  1 -  kelvin (k)');
-          Writeln('  2 -  Celsius (C)');
-          Writeln('  3 -  Fahrenheit (F)');
-        End;
+        begin
+          WriteLn(' TEMPERATURE' + sLineBreak);
+          WriteLn('  1 -  kelvin (k)');
+          WriteLn('  2 -  Celsius (C)');
+          WriteLn('  3 -  Fahrenheit (F)');
+        end;
     11:
-        Begin
-          Writeln(' ENERGY' + sLineBreak);
-          Writeln('  1 -   joule (J)');
-          Writeln('  2 -   kilojoule (kJ)');
-          Writeln('  3 -   kilocalorie (kcal)');
-          Writeln('  4 -   kilowatt per hour (kW/h)');
-        End;
+        begin
+          WriteLn(' ENERGY' + sLineBreak);
+          WriteLn('  1 -   joule (J)');
+          WriteLn('  2 -   kilojoule (kJ)');
+          WriteLn('  3 -   kilocalorie (kcal)');
+          WriteLn('  4 -   kilowatt per hour (kW/h)');
+        end;
     12:
-        Begin
-          Writeln(' POWER' + sLineBreak);
-          Writeln('  1 - watt (W)');
-          Writeln('  2 - kilojoule per hour (kJ/h)');
-          Writeln('  3 - kilocalorie per hour (kcal/h)');
-          Writeln('  4 - hoursepower (hp)');
-          Writeln('  5 - kilowatt (kW)');
-        End;
-  End;
-End;
+        begin
+          WriteLn(' POWER' + sLineBreak);
+          WriteLn('  1 - watt (W)');
+          WriteLn('  2 - kilojoule per hour (kJ/h)');
+          WriteLn('  3 - kilocalorie per hour (kcal/h)');
+          WriteLn('  4 - hoursepower (hp)');
+          WriteLn('  5 - kilowatt (kW)');
+        end;
+  end;
+end;
 
-Procedure Options();
-Begin
-  Writeln(' OPTIONS' + sLineBreak);
-  Writeln('  1 -  Do another conversion');
-  Writeln('  2 -  Return to the menu');
-  Writeln('  3 -  Return to the main menu');
-  Writeln('  4 -  Quit');
-End;
+procedure Options();
+begin
+  WriteLn(' OPTIONS' + sLineBreak);
+  WriteLn('  1 -  Do another conversion');
+  WriteLn('  2 -  Return to the menu');
+  WriteLn('  3 -  Return to the main menu');
+  WriteLn('  4 -  Quit');
+end;
 
 Function Converter(Opcode, InputUnit, OutputUnit: LongInt; Input: Real): Real;
-Begin
-  Case Opcode Of 
+begin
+  case Opcode of 
     1: Converter := Length(InputUnit, OutputUnit, Input);
     2: Converter := Area(InputUnit, OutputUnit, Input);
     3: Converter := Volume(InputUnit, OutputUnit, Input);
@@ -167,61 +167,61 @@ Begin
     10: Converter := Temperature(InputUnit, OutputUnit, Input);
     11: Converter := Energy(InputUnit, OutputUnit, Input);
     12: Converter := PowerC(InputUnit, OutputUnit, Input);
-  End;
-End;
+  end;
+end;
 
 
 Var 
   Opcode, Option, InputUnit, OutputUnit: LongInt;
   Input, Output: Real;
 
-Begin
+begin
   Write(sLineBreak);
-  Repeat
+  repeat
     MainMenu();
     Write(' Enter a code: ');
     Read(Opcode);
     Write(sLineBreak, sLineBreak);
-    If (Opcode = 13) Then Exit;
-    If ((Opcode < 1) Or (Opcode > 13)) Then
-    Begin
-      Writeln('Invalid option!', sLineBreak);
+    if (Opcode = 13) then exit;
+    if ((Opcode < 1) or (Opcode > 13)) then
+    begin
+      WriteLn('Invalid option!', sLineBreak);
       Option := 3;
-      Continue;
-    End;
-    Repeat
+      continue;
+    end;
+    repeat
       Menus(Opcode);
       Write(sLineBreak + ' Convert from: ');
       Read(InputUnit);
       Write(' to: ');
       Read(OutputUnit);
-      Repeat
+      repeat
         Write(' Enter the value you want to convert: ');
         Read(Input);
         Write(sLineBreak);
-        Try
+        try
           Output := Converter(Opcode, InputUnit, OutputUnit, Input);
           Write(' Result = ');
-          If (Output > 1E-3) And (Output < 1E6) Then Write(Output:10:3)
-          Else Write(Output);
+          if (Output > 1E-3) And (Output < 1E6) then Write(Output:10:3)
+          else Write(Output);
           Write(sLineBreak, sLineBreak);
-        Except
-          On E: Exception Do
-            Begin
-              Writeln('An error was encountered, or an invalid option was selected.');
-              Writeln('Error: ', E.Message, sLineBreak);
-            End;
-        End;
+        except
+          on E: Exception do
+            begin
+              WriteLn('An error was encountered, or an invalid option was selected.');
+              WriteLn('Error: ', E.Message, sLineBreak);
+            end;
+        end;
         Options();
         Write(sLineBreak, ' Enter an option: ');
         Read(Option);
         Write(sLineBreak, sLineBreak);
-        If ((Option <= 0) Or (Option > 4)) Then
-        Begin
-          Writeln('Invalid Option, exiting the program!', sLineBreak);
-          Exit;
-        End;
-      Until (Option <> 1);
-    Until (Option <> 2);
-  Until (Option <> 3);
-End.
+        if ((Option <= 0) or (Option > 4)) then
+        begin
+          WriteLn('Invalid Option, exiting the program!', sLineBreak);
+          exit;
+        end;
+      until (Option <> 1);
+    until (Option <> 2);
+  until (Option <> 3);
+end.
